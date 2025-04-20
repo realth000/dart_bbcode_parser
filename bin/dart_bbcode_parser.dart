df87940901f2f71449363a059b949ff8ffcd6e65
realth000 <a598919335@gmail.com>
2025-04-20 20:08:51 +0800

    feat(cmd): allow lex-only and parse-only processing

--- bin/dart_bbcode_parser.dart
+++ bin/dart_bbcode_parser.dart
@@ -1,17 +1,15 @@
 // Cli printing messages.
 // ignore_for_file: avoid_print
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
-import 'package:dart_bbcode_parser/dart_bbcode_parser.dart';
 import 'package:dart_bbcode_parser/src/dart_bbcode_parser.dart';
 import 'package:dart_bbcode_parser/src/lexer.dart';
 import 'package:dart_bbcode_parser/src/parser.dart';
 import 'package:dart_bbcode_parser/src/quill/delta.dart';
-import 'package:dart_bbcode_parser/src/tags/bold.dart';
-import 'package:dart_bbcode_parser/src/tags/italic.dart';
-import 'package:dart_bbcode_parser/src/tags/url.dart';
+
+const flagLexOnly = 'lex';
+const flagParseOnly = 'parse';
 
 Future<int> main(List<String> args) async {
   if (args.isEmpty) {
@@ -19,26 +17,38 @@ Future<int> main(List<String> args) async {
     return 0;
   }
 
-  final parser =
+  final argParser =
       ArgParser()
         ..addOption('code', abbr: 'c', help: 'parse bbcode from stdin', valueHelp: 'bbcode')
-        ..addOption('file', abbr: 'f', help: 'read and parse bbcode from file', valueHelp: 'file_path');
+        ..addOption('file', abbr: 'f', help: 'read and parse bbcode from file', valueHelp: 'file_path')
+        ..addFlag(flagLexOnly, abbr: 'l', help: 'process till lex end and print lexed result (tokens)')
+        ..addFlag(flagParseOnly, abbr: 'p', help: 'process till parse end and print parsed result (bbcode)');
 
-  final result = parser.parse(args);
+  final parsedArgs = argParser.parse(args);
 
-  if (result.option('code') != null) {
-    final delta = parseBBCodeTextToDelta(result.option('code')!);
+  if (parsedArgs.option('code') != null) {
+    final delta = parseBBCodeTextToDelta(parsedArgs.option('code')!);
     print('$delta');
     return 0;
   }
 
-  if (result.option('file') != null) {
-    final content = await File(result.option('file')!).readAsString();
-    final delta = parseBBCodeTextToDelta(content);
+  if (parsedArgs.option('file') != null) {
+    final content = await File(parsedArgs.option('file')!).readAsString();
+    final lexer = Lexer(input: content)..scanAll();
+    if (parsedArgs.flag(flagLexOnly)) {
+      print(lexer.tokens);
+      return 0;
+    }
+    final parser = Parser(tokens: lexer.tokens, supportedTags: defaultSupportedTags)..parse();
+    if (parsedArgs.flag(flagParseOnly)) {
+      print(parser.ast);
+      return 0;
+    }
+    final delta = buildDelta(parser.ast);
     print('$delta');
     return 0;
   }
 
-  print(parser.usage);
+  print(argParser.usage);
   return 1;
 }
