0562163baabfeb33244449cd0e290c97dfcceba1
realth000 <a598919335@gmail.com>
2025-06-18 01:58:53 +0800

    test(underline): do not allow non-empty attribute
    Added test.

---
+++ test/tags/underline_tag_test.dart
@@ -0,0 +1,105 @@
+import 'package:dart_bbcode_parser/src/tags/text.dart';
+import 'package:dart_bbcode_parser/src/tags/underline.dart';
+import 'package:dart_bbcode_parser/src/token.dart';
+import 'package:dart_quill_delta/dart_quill_delta.dart';
+import 'package:test/test.dart';
+
+import '../utils.dart';
+
+void main() {
+  group('underline tag', () {
+    test('without content', () {
+      const tag = 'u';
+      const head = '[$tag]';
+      const content = '';
+      const tail = '[/$tag]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: tag, attribute: null),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: tag,
+          ),
+        ],
+        expectedAST: [const UnderlineTag(start: 0, end: head.length + content.length + tail.length)],
+        expectedDelta: [
+          Operation.insert(content, {UnderlineTag.empty.quillAttrName: true}),
+          Operation.insert('\n'),
+        ],
+      );
+    });
+
+    test('with content', () {
+      const tag = 'u';
+      const head = '[$tag]';
+      const content = 'CONTENT';
+      const tail = '[/$tag]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: tag, attribute: null),
+          const Text(start: head.length, end: head.length + content.length, data: content),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: tag,
+          ),
+        ],
+        expectedAST: [
+          const UnderlineTag(
+            start: 0,
+            end: head.length + content.length + tail.length,
+            children: [TextContent(start: head.length, end: head.length + content.length, data: content)],
+          ),
+        ],
+        expectedDelta: [
+          Operation.insert(content, {UnderlineTag.empty.quillAttrName: true}),
+          Operation.insert('\n'),
+        ],
+      );
+    });
+
+    test('invalid with attribute', () {
+      const tag = 'u';
+      const attr = 'attr';
+      const head = '[$tag=$attr]';
+      const content = 'CONTENT';
+      const tail = '[/$tag]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: tag, attribute: attr),
+          const Text(start: head.length, end: head.length + content.length, data: content),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: tag,
+          ),
+        ],
+        expectedAST: [
+          const TextContent(start: 0, end: head.length, data: head),
+          const TextContent(start: head.length, end: head.length + content.length, data: content),
+          const TextContent(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            data: tail,
+          ),
+        ],
+        expectedDelta: [
+          Operation.insert(head, {}),
+          Operation.insert(content, {}),
+          Operation.insert(tail, {}),
+          Operation.insert('\n'),
+        ],
+      );
+    });
+  });
+}
