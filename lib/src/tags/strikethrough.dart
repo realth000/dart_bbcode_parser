56ba275fa5e80fc59043475e5dff6a4f6b4bb901
realth000 <a598919335@gmail.com>
2025-06-18 01:53:09 +0800

    fix(strikethrough): do not allow non-empty attribute in strikethrough
    Added test.

--- lib/src/tags/strikethrough.dart
+++ lib/src/tags/strikethrough.dart
@@ -1,3 +1,4 @@
+import 'package:dart_bbcode_parser/dart_bbcode_parser.dart';
 import 'package:dart_bbcode_parser/src/tags/common_tag.dart';
 import 'package:dart_bbcode_parser/src/tags/tag.dart';
 import 'package:dart_bbcode_parser/src/token.dart';
@@ -9,6 +10,9 @@ class StrikethroughTag extends NoAttrTag {
 
   /// Build empty one.
   static const empty = StrikethroughTag(start: -1, end: -1);
+
+  @override
+  AttributeValidator? get attributeValidator => nullAttributeValidator;
 
   @override
   String get name => 's';
