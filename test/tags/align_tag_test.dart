c77ac127da5d2f0c42d13a8005bcf34e1e390103
realth000 <a598919335@gmail.com>
2025-06-17 21:57:41 +0800

    test: optimize test

--- test/tags/align_tag_test.dart
+++ test/tags/align_tag_test.dart
@@ -1,185 +1,189 @@
-import 'package:dart_bbcode_parser/src/dart_bbcode_parser.dart';
-import 'package:dart_bbcode_parser/src/lexer.dart';
-import 'package:dart_bbcode_parser/src/parser.dart';
-import 'package:dart_bbcode_parser/src/quill/delta.dart';
 import 'package:dart_bbcode_parser/src/tags/align.dart';
 import 'package:dart_bbcode_parser/src/tags/text.dart';
 import 'package:dart_bbcode_parser/src/token.dart';
 import 'package:dart_quill_delta/dart_quill_delta.dart';
 import 'package:test/test.dart';
 
+import '../utils.dart';
+
 void main() {
   group('align center tag', () {
     test('without content', () {
-      const input = '[align=center][/align]';
-      final lexer = Lexer(input: input)..scanAll();
-      final tokens = lexer.tokens;
-      expect(tokens.length, 2);
-      expect(tokens[0], const TagHead(start: 0, end: 14, name: 'align', attribute: 'center'));
-      expect(tokens[1], const TagTail(start: 14, end: 22, name: 'align'));
-
-      final parser = Parser(tokens: tokens, supportedTags: defaultSupportedTags)..parse();
-      final ast = parser.ast;
-      expect(ast.length, 1);
-      expect(ast[0], const AlignTag(start: 0, end: 22, attribute: 'center'));
-
-      final quillDelta = buildDelta(ast);
-      final targetDelta = Delta.fromOperations([
-        Operation.insert('', {}),
-        Operation.insert('\n', {'align': 'center'}),
-      ]);
-      expect(quillDelta.toJson(), targetDelta.toJson());
-
-      expect(convertBBCodeToText(ast), input);
+      const head = '[align=center]';
+      const content = '';
+      const tail = '[/align]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: 'align', attribute: 'center'),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: 'align',
+          ),
+        ],
+        expectedBBCodeTags: [
+          const AlignTag(start: 0, end: head.length + content.length + tail.length, attribute: 'center'),
+        ],
+        expectedOperations: [
+          Operation.insert('', {}),
+          Operation.insert('\n', {'align': 'center'}),
+        ],
+      );
     });
 
     test('with content', () {
+      const head = '[align=center]';
       const content = 'CONTENT';
-      const input = '[align=center]$content[/align]';
-      final lexer = Lexer(input: input)..scanAll();
-      final tokens = lexer.tokens;
-      expect(tokens.length, 3);
-      expect(tokens[0], const TagHead(start: 0, end: 14, name: 'align', attribute: 'center'));
-      expect(tokens[1].tokenType, TokenType.text);
-      expect(tokens[2], const TagTail(start: 14 + content.length, end: 22 + content.length, name: 'align'));
-
-      final parser = Parser(tokens: tokens, supportedTags: defaultSupportedTags)..parse();
-      final ast = parser.ast;
-      expect(ast.length, 1);
-      expect(
-        ast[0],
-        const AlignTag(
-          start: 0,
-          end: 22 + content.length,
-          attribute: 'center',
-          children: [TextContent(14, 14 + content.length + 1, content)],
-        ),
+      const tail = '[/align]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: 'align', attribute: 'center'),
+          const Text(start: head.length, end: head.length + content.length + 1, data: content),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: 'align',
+          ),
+        ],
+        expectedBBCodeTags: [
+          const AlignTag(
+            start: 0,
+            end: head.length + content.length + tail.length,
+            attribute: 'center',
+            children: [TextContent(head.length, head.length + content.length + 1, content)],
+          ),
+        ],
+        expectedOperations: [
+          Operation.insert(content, {}),
+          Operation.insert('\n', {'align': 'center'}),
+        ],
       );
-
-      final quillDelta = buildDelta(ast);
-      final targetDelta = Delta.fromOperations([
-        Operation.insert(content, {}),
-        Operation.insert('\n', {'align': 'center'}),
-      ]);
-      expect(quillDelta.toJson(), targetDelta.toJson());
-
-      expect(convertBBCodeToText(ast), input);
     });
   });
 
   group('align left tag', () {
     test('without content', () {
-      const input = '[align=left][/align]';
-      final lexer = Lexer(input: input)..scanAll();
-      final tokens = lexer.tokens;
-      expect(tokens.length, 2);
-      expect(tokens[0], const TagHead(start: 0, end: 12, name: 'align', attribute: 'left'));
-      expect(tokens[1], const TagTail(start: 12, end: 20, name: 'align'));
-
-      final parser = Parser(tokens: tokens, supportedTags: defaultSupportedTags)..parse();
-      final ast = parser.ast;
-      expect(ast.length, 1);
-      expect(ast[0], const AlignTag(start: 0, end: 20, attribute: 'left'));
-
-      final quillDelta = buildDelta(ast);
-      final targetDelta = Delta.fromOperations([
-        Operation.insert('', {}),
-        Operation.insert('\n', {'align': 'left'}),
-      ]);
-      expect(quillDelta.toJson(), targetDelta.toJson());
-
-      expect(convertBBCodeToText(ast), input);
+      const head = '[align=left]';
+      const content = '';
+      const tail = '[/align]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: 'align', attribute: 'left'),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: 'align',
+          ),
+        ],
+        expectedBBCodeTags: [
+          const AlignTag(start: 0, end: head.length + content.length + tail.length, attribute: 'left'),
+        ],
+        expectedOperations: [
+          Operation.insert('', {}),
+          Operation.insert('\n', {'align': 'left'}),
+        ],
+      );
     });
 
     test('with content', () {
+      const head = '[align=left]';
       const content = 'CONTENT';
-      const input = '[align=left]$content[/align]';
-      final lexer = Lexer(input: input)..scanAll();
-      final tokens = lexer.tokens;
-      expect(tokens.length, 3);
-      expect(tokens[0], const TagHead(start: 0, end: 12, name: 'align', attribute: 'left'));
-      expect(tokens[1].tokenType, TokenType.text);
-      expect(tokens[2], const TagTail(start: 12 + content.length, end: 20 + content.length, name: 'align'));
-
-      final parser = Parser(tokens: tokens, supportedTags: defaultSupportedTags)..parse();
-      final ast = parser.ast;
-      expect(ast.length, 1);
-      expect(
-        ast[0],
-        const AlignTag(
-          start: 0,
-          end: 20 + content.length,
-          attribute: 'left',
-          children: [TextContent(12, 12 + content.length + 1, content)],
-        ),
+      const tail = '[/align]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: 'align', attribute: 'left'),
+          const Text(start: head.length, end: head.length + content.length + 1, data: content),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: 'align',
+          ),
+        ],
+        expectedBBCodeTags: [
+          const AlignTag(
+            start: 0,
+            end: head.length + content.length + tail.length,
+            attribute: 'left',
+            children: [TextContent(head.length, head.length + content.length + 1, content)],
+          ),
+        ],
+        expectedOperations: [
+          Operation.insert(content, {}),
+          Operation.insert('\n', {'align': 'left'}),
+        ],
       );
-
-      final quillDelta = buildDelta(ast);
-      final targetDelta = Delta.fromOperations([
-        Operation.insert(content, {}),
-        Operation.insert('\n', {'align': 'left'}),
-      ]);
-      expect(quillDelta.toJson(), targetDelta.toJson());
-
-      expect(convertBBCodeToText(ast), input);
     });
   });
 
   group('align right tag', () {
     test('without content', () {
-      const input = '[align=right][/align]';
-      final lexer = Lexer(input: input)..scanAll();
-      final tokens = lexer.tokens;
-      expect(tokens.length, 2);
-      expect(tokens[0], const TagHead(start: 0, end: 13, name: 'align', attribute: 'right'));
-      expect(tokens[1], const TagTail(start: 13, end: 21, name: 'align'));
-
-      final parser = Parser(tokens: tokens, supportedTags: defaultSupportedTags)..parse();
-      final ast = parser.ast;
-      expect(ast.length, 1);
-      expect(ast[0], const AlignTag(start: 0, end: 21, attribute: 'right'));
-
-      final quillDelta = buildDelta(ast);
-      final targetDelta = Delta.fromOperations([
-        Operation.insert('', {}),
-        Operation.insert('\n', {'align': 'right'}),
-      ]);
-      expect(quillDelta.toJson(), targetDelta.toJson());
-
-      expect(convertBBCodeToText(ast), input);
+      const head = '[align=right]';
+      const content = '';
+      const tail = '[/align]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: 'align', attribute: 'right'),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: 'align',
+          ),
+        ],
+        expectedBBCodeTags: [
+          const AlignTag(start: 0, end: head.length + content.length + tail.length, attribute: 'right'),
+        ],
+        expectedOperations: [
+          Operation.insert('', {}),
+          Operation.insert('\n', {'align': 'right'}),
+        ],
+      );
     });
 
     test('with content', () {
+      const head = '[align=right]';
       const content = 'CONTENT';
-      const input = '[align=right]$content[/align]';
-      final lexer = Lexer(input: input)..scanAll();
-      final tokens = lexer.tokens;
-      expect(tokens.length, 3);
-      expect(tokens[0], const TagHead(start: 0, end: 13, name: 'align', attribute: 'right'));
-      expect(tokens[1].tokenType, TokenType.text);
-      expect(tokens[2], const TagTail(start: 13 + content.length, end: 21 + content.length, name: 'align'));
-
-      final parser = Parser(tokens: tokens, supportedTags: defaultSupportedTags)..parse();
-      final ast = parser.ast;
-      expect(ast.length, 1);
-      expect(
-        ast[0],
-        const AlignTag(
-          start: 0,
-          end: 21 + content.length,
-          attribute: 'right',
-          children: [TextContent(13, 13 + content.length + 1, content)],
-        ),
+      const tail = '[/align]';
+      checkSingleTag(
+        head: head,
+        content: content,
+        tail: tail,
+        expectedTokens: [
+          const TagHead(start: 0, end: head.length, name: 'align', attribute: 'right'),
+          const Text(start: head.length, end: head.length + content.length + 1, data: content),
+          const TagTail(
+            start: head.length + content.length,
+            end: head.length + content.length + tail.length,
+            name: 'align',
+          ),
+        ],
+        expectedBBCodeTags: [
+          const AlignTag(
+            start: 0,
+            end: head.length + content.length + tail.length,
+            attribute: 'right',
+            children: [TextContent(head.length, head.length + content.length + 1, content)],
+          ),
+        ],
+        expectedOperations: [
+          Operation.insert(content, {}),
+          Operation.insert('\n', {'align': 'right'}),
+        ],
       );
-
-      final quillDelta = buildDelta(ast);
-      final targetDelta = Delta.fromOperations([
-        Operation.insert(content, {}),
-        Operation.insert('\n', {'align': 'right'}),
-      ]);
-      expect(quillDelta.toJson(), targetDelta.toJson());
-
-      expect(convertBBCodeToText(ast), input);
     });
   });
 }
---
+++ test/utils.dart
@@ -0,0 +1,50 @@
+import 'package:dart_bbcode_parser/src/dart_bbcode_parser.dart';
+import 'package:dart_bbcode_parser/src/lexer.dart';
+import 'package:dart_bbcode_parser/src/parser.dart';
+import 'package:dart_bbcode_parser/src/quill/delta.dart';
+import 'package:dart_bbcode_parser/src/tags/tag.dart';
+import 'package:dart_bbcode_parser/src/token.dart';
+import 'package:dart_quill_delta/dart_quill_delta.dart';
+import 'package:test/expect.dart';
+
+void checkSingleTag({
+  required String head,
+  required String content,
+  required String tail,
+  required List<Token> expectedTokens,
+  required List<BBCodeTag> expectedBBCodeTags,
+  required List<Operation> expectedOperations,
+  List<BBCodeTag> supportedTags = defaultSupportedTags,
+  bool checkTokens = true,
+  bool checkAST = true,
+  bool checkDelta = true,
+  bool checkBBCode = true,
+}) {
+  final input = '$head$content$tail';
+
+  // Tokens stage.
+  final lexer = Lexer(input: input)..scanAll();
+  final tokens = lexer.tokens;
+  if (checkTokens) {
+    expect(tokens, equals(expectedTokens), reason: 'tokens not match as expected');
+  }
+
+  // AST stage.
+  final parser = Parser(tokens: tokens, supportedTags: supportedTags)..parse();
+  final ast = parser.ast;
+  if (checkAST) {
+    expect(ast, equals(expectedBBCodeTags), reason: 'AST not match as expected');
+  }
+
+  // Delta stage.
+  final delta = buildDelta(ast);
+  final targetDelta = Delta.fromOperations(expectedOperations);
+  if (checkDelta) {
+    expect(delta.toJson(), equals(targetDelta.toJson()), reason: 'delta not match as expected');
+  }
+
+  // Back to BBCode stage.
+  if (checkBBCode) {
+    expect(convertBBCodeToText(ast), equals(input), reason: 'BBCode not match as expected');
+  }
+}
