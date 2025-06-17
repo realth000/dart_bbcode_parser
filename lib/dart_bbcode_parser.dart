69b76ceb9b2121c61708adc65b8a8aa37ebe8a5d
realth000 <a598919335@gmail.com>
2025-06-18 00:16:08 +0800

    feat: export utility attribute validator

--- lib/dart_bbcode_parser.dart
+++ lib/dart_bbcode_parser.dart
@@ -1,5 +1,5 @@
 /// Parsing bbcode in pure dart.
 library;
 
-// For test.
 export 'src/dart_bbcode_parser.dart' show convertBBCodeToText, parseBBCodeTextToDelta, parseBBCodeTextToTags;
+export 'src/utils.dart' show AttributeValidator, nullAttributeValidator;
--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,5 +3,8 @@ import 'package:dart_bbcode_parser/src/tags/tag.dart';
 /// Function validating tag attributes.
 typedef AttributeValidator = bool Function(String? input);
 
+/// Use this validator when attribute is not allowed.
+AttributeValidator nullAttributeValidator = (attr) => attr == null;
+
 /// Function validating tag children contents.
 typedef ChildrenValidator = bool Function(List<BBCodeTag> input);
