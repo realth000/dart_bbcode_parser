735f704c9cb20078c23ff0c714dd56a222bb54df
realth000 <a598919335@gmail.com>
2025-06-19 02:38:08 +0800

    test: add test for basic tag

---
+++ test/tags/tag_test.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:dart_bbcode_parser/src/constants.dart';
+import 'package:dart_bbcode_parser/src/tags/bold.dart';
+import 'package:dart_bbcode_parser/src/tags/tag.dart';
+import 'package:test/test.dart';
+
+import '../utils.dart';
+
+void main() {
+  group('abstract basic tag', () {
+    test('unused situations', () {
+      final tag = buildSingleTag(input: '[b][/b]');
+      expect(tag.runtimeType, equals(BoldTag));
+      final tagData = {
+        'start': 0,
+        'end': 7,
+        'open': '[',
+        'close': ']',
+        'name': 'b',
+        'selfClosed': false,
+        'selfClosedAtTail': false,
+        'hasQuillAttr': true,
+        'quillAttrName': 'bold',
+        'quillAttrValue': true,
+        'hasQuillEmbed': false,
+        'quillEmbedName': K.unsupported,
+        'quillEmbedValue': K.unsupported,
+        'target': '${ApplyTarget.text}',
+        'attribute': null,
+        'children': '',
+      };
+      expect(tag.toJson(), equals(tagData));
+      expect(tag.toString(), equals(jsonEncode(tagData)));
+      expect(tag.hashCode.runtimeType, equals(int));
+    });
+  });
+}
